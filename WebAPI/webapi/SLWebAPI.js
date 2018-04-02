70bea1a98c5452877e03c3bc30a9a4a27100c27d
Long Nguyen <LongNguyen@dn-minimac02.local>
2018-04-02 14:27:29 +0700

    refactor

--- WebAPI/webapi/SLWebAPI.js
+++ WebAPI/webapi/SLWebAPI.js
@@ -1,61 +1,20 @@
 
 export default class SLWebAPI {
-    constructor(baseUrl = '', { headers = {}} = {}) {
-        if (!baseUrl) throw new Error('missing baseUrl');
-        this.headers = {
+    static request(method, url, body, headers){
+        var optHeader = {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
             'Authorization':'',
-            'Accept-Language': 'en-EN'
+            'Accept-Language': 'en-US'
         };
-        Object.assign(this.headers, headers);
-        this.baseUrl = baseUrl;       
-    }
-    getFullURL(url) {
-        return `${this.baseUrl}${url}`;
-    }
-    
-    GET(route) { 
-        var fullRoute = this.getFullURL(route);
+        Object.assign(optHeader, headers);
         let opts = {
-            method :'GET',
-            headers: this.headers
-        };
-        const fetchPromise = () => fetch(fullRoute, opts);        
-        return fetchPromise()
-            .then(response => response.json());        
-    }
-    POST(route, body) { 
-        var fullRoute = this.getFullURL(route);
-        let opts = {
-            method :'POST',
-            headers: this.headers
+            method : method,
+            headers: optHeader
         };
         Object.assign(opts, { body: JSON.stringify(body) });
         const fetchPromise = () => fetch(fullRoute, opts);        
         return fetchPromise()
-            .then(response => response.json());        
+            .then(response => response.json());  
     }
-    PUT(route, body) { 
-        var fullRoute = this.getFullURL(route);
-        let opts = {
-            method :'PUT',
-            headers: this.headers
-        };
-        Object.assign(opts, { body: JSON.stringify(body) });
-        const fetchPromise = () => fetch(fullRoute, opts);        
-        return fetchPromise()
-            .then(response => response.json());        
-    }
-    DELETE(route) { 
-        var fullRoute = this.getFullURL(route);
-        let opts = {
-            method :'DELETE',
-            headers: this.headers
-        };
-        const fetchPromise = () => fetch(fullRoute, opts);        
-        return fetchPromise()
-            .then(response => response.json());        
-    }
-
 }
